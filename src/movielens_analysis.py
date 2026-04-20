449fa8906db85bdf40691e6a1268dc3fe19d1b20
annadymovaa <aanadumova@hse.edu.ru>
2026-04-20 23:18:25 +0500

    finished Ratings

--- src/movielens_analysis.py
+++ src/movielens_analysis.py
@@ -7,152 +7,176 @@ class Ratings:
     """
     Analyzing data from ratings.csv
     """
-    def __init__(self, path_to_the_file='../datasets/ml-latest-small-1000/ratings_1000.csv'):
-        self.filepath = path_to_the_file 
-        self.movies = self.Users(self)
+    def __init__(self, path='../datasets/ml-latest-small-1000/ratings_1000.csv'):
+        if not os.path.isfile(path) or os.path.getsize(path) == 0:
+            raise FileNotFoundError(f"Файл не найден или пустой: {path}")
+        
+        self.filepath = path
         self.get_content()
+        self.users = self.Users(self)
 
     def get_content(self):
         with open(self.filepath, 'r', encoding='utf-8') as file:
-            self.content = file.readlines()
+            content = file.readlines()
+        movies = Movies()
+        self.content = list()
+        for i in content[1:]:
+            line = i.split(',')
+            line_dict = {
+                'userId' : line[0].strip(),
+                'movieId' : line[1].strip(),
+                'rating' : float(line[2].strip()),
+                'timestamp' : int(line[3].strip()),
+                'title' : None
+            }
+            
+            for movie in movies.movies_data:
+                if str(line_dict['movieId']) == str(movie['movieId']):
+                    line_dict['title'] = movie['title']
+                    break
+            self.content.append(line_dict)
+
 
     class Movies:  
         def __init__(self, outer_instance):
-            # Сохраняем ссылку на внешний объект
+            #сохраняем ссылку на внешний объект
             self.outer = outer_instance  
 
-        def dist_by_year(self):
+        def dist_by_year(self, n=10):
             #{year : count} по ключам по возрастанию
 
             years = []
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                timestamp = int(line[3])
+            for line in self.outer.content:
+                timestamp = line['timestamp']
                 m_year = datetime.fromtimestamp(timestamp).year
                 years.append(m_year)
             count_by_year = Counter(years)
             ratings_by_year = dict(sorted(count_by_year.items()))
 
+            print("📅 КОЛИЧЕСТВО ОЦЕНОК ФИЛЬМОВ ПО ГОДАМ (1996 - 2015):")
+            for year, count in list(ratings_by_year.items())[:n]:
+                bar = "█" * (count // 5)
+                print(f"{year}: {bar} {count}")
             return ratings_by_year
         
         def dist_by_rating(self):
             #{rating : count} по рейтингу по возрастанию
 
             ratings = []
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                rating = float(line[2])
+            for line in self.outer.content:
+                rating = line['rating']
                 ratings.append(rating)
             count_by_rating = Counter(ratings)
             ratings_distribution = dict(sorted(count_by_rating.items()))
-
+            print("Распределение по количеству оценок:")
+            for rating, count in ratings_distribution.items():
+                print(f"⭐ {rating} — {count} оценок")
             return ratings_distribution
+                
+        def dict_of_mov_rats(self):
+            self.mov_rating = dict()
+            for line in self.outer.content:
+                key = (line['movieId'], line['title'])
+                if key not in self.mov_rating:
+                    self.mov_rating[key] = list()
+                self.mov_rating[key].append(line['rating'])
         
-        def top_by_num_of_ratings(self, n):
+        def top_by_num_of_ratings(self, n=10):
             #{movie_title : num of ratings} по номерам по убыванию
+            self.dict_of_mov_rats()
+            mov_rating = list()
+            for (movieId, title), ratings in self.mov_rating.items():
+                if title is not None:
+                    mov_rating.append((title, len(ratings)))
 
-            mov_id_rat = dict()
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                movie_id = line[1]
-                if movie_id not in mov_id_rat:
-                    mov_id_rat[movie_id] = 1
-                else:
-                    mov_id_rat[movie_id] += 1
-            movie_ids = dict(sorted(mov_id_rat.items(), key=lambda item: item[1], reverse=True))
+            movies = list(sorted(mov_rating, key=lambda item: item[1], reverse=True))
+            top_movies = dict(movies[:n])
 
-            top_movie_ids = dict(list(movie_ids.items())[:n])
-
-            # кажется, что для этого сойдет какой-нибудь метод из класса Movies
-            # пока что оставлю так
-            # with open('../datasets/ml-latest-small-1000/movies_1000.csv', 'r', encoding='utf-8') as file:
-            #     movies = file.readlines()
-            # for movie_id in top_movie_ids:
-            #     for movie in movies:
-            #         line = movie.split(',')
-            #         if movie_id == line[0]:
-
-            top_movies = top_movie_ids
+            print("🏆 ТОП-10 ФИЛЬМОВ ПО КОЛИЧЕСТВУ ОЦЕНОК:")
+            for title, metric in top_movies.items():
+                print(f"• {title} - {metric} оценки")
             return top_movies
         
-        def dict_of_ids_rats(self):
-            self.mov_id_rat = dict()
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                movie_id = line[1] 
-                rating = float(line[2])   
-                if movie_id not in self.mov_id_rat:
-                    self.mov_id_rat[movie_id] = list()
-                self.mov_id_rat[movie_id].append(rating)
-        
-        def top_by_ratings(self, n, metric='average'):
-            self.dict_of_ids_rats()
-            movie_id_metrics = dict()
-            for movie in self.mov_id_rat:
-                list_of_rats = self.mov_id_rat[movie]
+        def top_by_ratings(self, n=10, metric='average'):
+            self.dict_of_mov_rats()
+            movie_metrics = [] 
+
+            for (m_id, title), list_of_rats in self.mov_rating.items():
+                if title is None: continue  # Пропускаем фильмы без названий
+                
+                num = len(list_of_rats)
                 if metric == 'average':
-                    num = len(list_of_rats)
-                    if num != 1:
-                        metric_v = sum(list_of_rats) / len(list_of_rats)
-                    else:
-                        metric_v = list_of_rats[0]
+                    metric_v = sum(list_of_rats) / num
                 elif metric == 'median':
-                    num = len(list_of_rats)
-                    if num != 1:
-                        sorted_rating = sorted(list_of_rats)
-                        mid = num // 2
-                        if num % 2 == 1:
-                            metric_v = sorted_rating[mid]
-                        else:
-                            metric_v = (sorted_rating[mid - 1] + sorted_rating[mid]) / 2
+                    sorted_rating = sorted(list_of_rats)
+                    mid = num // 2
+                    if num % 2 == 1:
+                        metric_v = sorted_rating[mid]
                     else:
-                        metric_v = list_of_rats[0]
+                        metric_v = (sorted_rating[mid - 1] + sorted_rating[mid]) / 2
                 else:
                     raise Exception('The metric is wrong! Choose between average and median')
+                
+                movie_metrics.append((title, round(metric_v, 2)))
             
-                movie_id_metrics[movie] = round(metric_v, 2)
-            
-            movie_ids = dict(sorted(movie_id_metrics.items(), key=lambda item: item[1], reverse=True))
-            top_movie_ids = dict(list(movie_ids.items())[:n])
-            
-            top_movies = top_movie_ids
-            #TODO: добавить замену айди на название фильма
-              
-            """
-            The method returns top-n movies by the average or median of the ratings.
-            It is a dict where the keys are movie titles and the values are metric values.
-            Sort it by metric descendingly.
-            The values should be rounded to 2 decimals.
-            """
+            movie_metrics.sort(key=lambda x: x[1], reverse=True)
+            top_movies = dict(movie_metrics[:n])
+
+            print(f"🏆 ТОП-10 ФИЛЬМОВ ПО {metric.upper()}:")
+            place = 1
+            for title, metric_v in top_movies.items():
+                print(f"{place}. {title} ({metric_v} ⭐)")
+                place += 1
             return top_movies
-        def top_controversial(self, n):
-            self.dict_of_ids_rats()
-            movie_id_metrics = dict()
-            for movie in self.mov_id_rat:
-                list_of_rats = self.mov_id_rat[movie]
-                variance = 0
+
+        def top_controversial(self, n=10):
+            self.dict_of_mov_rats()
+            movie_metrics = []
+
+            for (m_id, title), list_of_rats in self.mov_rating.items():
+                if title is None: continue
+                
                 rat_count = len(list_of_rats)
-                if rat_count != 1:
-                    cumulator = 0
+                if rat_count < 2: # Дисперсия одного числа всегда 0
+                    variance = 0
+                else:
                     mean = sum(list_of_rats) / rat_count
-                    for rating in list_of_rats:
-                        cumulator += (rating - mean) ** 2
+                    cumulator = sum((rating - mean) ** 2 for rating in list_of_rats)
                     variance = cumulator / rat_count
-                movie_id_metrics[movie] = round(variance, 2)
+                    
+                movie_metrics.append((title, round(variance, 2)))
 
-            movie_ids = dict(sorted(movie_id_metrics.items(), key=lambda item: item[1], reverse=True))
-            top_movie_ids = dict(list(movie_ids.items())[:n])
-            
-            top_movies = top_movie_ids
-            #TODO: добавить замену айди на название фильма
+            movie_metrics.sort(key=lambda x: x[1], reverse=True)
+            controversial_movies = dict(movie_metrics[:n])
 
-            """
-            The method returns top-n movies by the variance of the ratings.
-            It is a dict where the keys are movie titles and the values are the variances.
-          Sort it by variance descendingly.
-            The values should be rounded to 2 decimals.
-            """
-            return top_movies
+            place = 1
+            for title, variance in controversial_movies.items():
+                print(f'{place} место : {title} (дисперсия оценок {variance})')
+                place += 1
+
+            return controversial_movies
+
+        
+        def top_by_max_ratings(self, n=1):
+            self.dict_of_mov_rats()
+            max_rating_count = []
+
+            for (m_id, title), ratings in self.mov_rating.items():
+                if title is None: continue
+                
+                count_max = ratings.count(5.0)
+                if count_max > 0:
+                    max_rating_count.append((title, count_max))
+
+            max_rating_count.sort(key=lambda x: x[1], reverse=True)
+            top_favorite_movie = dict(max_rating_count[:n])
+
+            place = 1
+            for title, count in top_favorite_movie.items():
+                print(f'{place} : {title} (кол-во отличных оценок {count})')
+                place += 1
+            return top_favorite_movie
+
 
     class Users(Movies):
         """
@@ -167,21 +191,22 @@ class Ratings:
             #{user : count} по кол-ву оценок по убыванию
 
             users = []
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                user = line[0]
+            for line in self.outer.content:
+                user = line['userId']
                 users.append(user)
             count_by_ratings = Counter(users)
             ratings_by_user = dict(sorted(count_by_ratings.items(), key=lambda item: item[1], reverse=True))
-
+            place = 1
+            for user, ratings in ratings_by_user.items():
+                print(f'{place} место : user {user} ({ratings} оценок)')
+                place += 1
             return ratings_by_user
         
         def dict_of_user_rats(self):
             self.user_id_rat = dict()
-            for i in range(1, 1001):
-                line = self.outer.content[i].split(',')
-                user_id = line[0] 
-                rating = float(line[2])   
+            for line in self.outer.content:
+                user_id = line['userId'] 
+                rating = line['rating']  
                 if user_id not in self.user_id_rat:
                     self.user_id_rat[user_id] = list()
                 self.user_id_rat[user_id].append(rating)
@@ -215,10 +240,14 @@ class Ratings:
                 user_id_metrics[user] = round(metric_v, 2)
             
             user_ids = dict(sorted(user_id_metrics.items(), key=lambda item: item[1], reverse=True))
-            
+            place = 1
+            print((f'\nТоп пользователей по {metric.upper()}:'))
+            for user, metric_v in user_ids.items():
+                print(f'{place} место : user {user} ({metric_v} ⭐)')
+                place += 1
             return user_ids
 
-        def top_controversial_users(self, n):
+        def top_controversial_users(self, n=3):
             self.dict_of_user_rats()
             user_id_metrics = dict()
             for movie in self.user_id_rat:
@@ -235,6 +264,8 @@ class Ratings:
 
             user_ids = dict(sorted(user_id_metrics.items(), key=lambda item: item[1], reverse=True))
             top_user_ids = dict(list(user_ids.items())[:n])
-
+            place = 1
+            for user, variance in top_user_ids.items():
+                print(f'{place} место : user {user} (дисперсия оценок {variance})')
+                place += 1
             return top_user_ids
-
